51695526087b093b9fdacffc402fc8edbf71c0ad
d883695 <adrian.mroczek@team.telstra.com>
2021-05-26 23:34:19 +0800

    more header items, responsive styling

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,18 @@ import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
+// NOTE: flex-grow uses up as much space as it can in viewport
+// Add max-w-2xl to stop it spreading the whole viewport
 function Header() {
   return (
-    <header className="">
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image src="https://links.papareact.com/ua6" width={200} height={100} />
     </header>
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({ title, Icon }) {
   return (
-    <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
+    <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-2 ml-1.5 mr-1.5 hover:text-white">
       <Icon className="h-8 mb-1 group-hover:animate-bounce" />
       <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
     </div>
