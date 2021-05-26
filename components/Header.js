dd857b1f07d819413fe3740b49cace5f85e07102
d883695 <adrian.mroczek@team.telstra.com>
2021-05-26 23:14:32 +0800

    classname fix, header setup, header item created

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon } from "@heroicons/react/outline";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+
 function Header() {
   return (
     <header className="">
-      <h1>this is the header</h1>
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <Image src="https://links.papareact.com/ua6" width={200} height={100} />
     </header>
   );
 }
