0b5340d6b7a6f0ca04dca9f3f505c168a914ee42
d883695 <adrian.mroczek@team.telstra.com>
2021-05-27 20:35:57 +0800

    nav scaffold, request mock ups

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <title>Hulu Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Nav />
       <Header />
     </div>
   );
