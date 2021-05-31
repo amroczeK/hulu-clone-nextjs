99b82737212962786bddbe6c108665e65528ded7
amroczeK <mroczek9990@gmail.com>
2021-05-31 23:08:27 +0800

    thumbnail responsive styling

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,18 +1,16 @@
 import Image from 'next/image';
-import { ThumbUpIcon } from '@heroicons/react/outline';
+import { ThumbUpIcon, StarIcon } from '@heroicons/react/outline';
 
 function Thumbnail({ result }) {
-  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+  const BASE_URL = 'https://image.tmdb.org/t/p/original';
+  console.log(result);
   return (
     <div className='group cursor-pointer p-2 transition ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
-        src={
-          `${BASE_URL}${result.backgrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
-        height={1080}
-        width={1920}
+        src={`${BASE_URL}${result.backdrop_path}`}
+        height={720}
+        width={1280}
       />
       <div className='p-2'>
         <p className='truncate max-w-md'>{result.overview}</p>
@@ -21,7 +19,8 @@ function Thumbnail({ result }) {
         </h2>
         <p className='flex items-center opacity-0 group-hover:opacity-100'>
           {result?.media_type && `${result?.media_type.toUpperCase()} •`}{' '}
-          {result?.release_date || result?.first_air_date} • <ThumbUpIcon className='h-5 mx-2' />
+          {result?.release_date || result?.first_air_date} • <StarIcon className='h-5 mx-2' />
+          {result?.vote_average} • <ThumbUpIcon className='h-5 mx-2' />
           {result?.vote_count}
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      '3xl': '1920px', // custom breakpoint
+    },
   },
   variants: {
     extend: {},
