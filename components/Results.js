9b0f3612f41c3ee38f34cf5eab2c5483c6d2d8cd
amroczeK <mroczek9990@gmail.com>
2021-05-31 21:12:24 +0800

    thumbnail basic styling

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,13 @@
-import requests from "../utils/requests";
+import Thumbnail from './Thumbnail';
 
 function Results({ results }) {
-  return <div></div>;
+  return (
+    <div>
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
